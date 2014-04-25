119e4b2dc89bce3a995957c99d79314110a153e4
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-04-25 21:02:55 +1000

    Refactor rtmp url parsing into separate method

--- resources/lib/play.py
+++ resources/lib/play.py
@@ -30,33 +30,36 @@ try:
 except ImportError:
     pass # for PC debugging
 
+def rtmp_url(url):
+    iview_config = comm.get_config()
+    auth = comm.get_auth(iview_config)
+
+    # We don't support Adobe HDS yet, Fallback to RTMP streaming server
+    if auth['rtmp_url'].startswith('http://'):
+        auth['rtmp_url'] = iview_config['rtmp_url'] or config.akamai_fallback_server
+        auth['playpath_prefix'] = config.akamai_playpath_prefix
+        utils.log("Adobe HDS Not Supported, using fallback server %s" % auth['rtmp_url'])
+
+    # Playpath shoud look like this:
+    #   Akamai: mp4:flash/playback/_definst_/itcrowd_10_03_02
+    playpath = auth['playpath_prefix'] + url
+    if playpath.split('.')[-1] == 'mp4':
+        playpath = 'mp4:' + playpath
+
+    # Strip off the .flv or .mp4
+    playpath = playpath.split('.')[0]
+
+    # rtmp://cp53909.edgefcs.net/ondemand?auth=daEbjbeaCbGcgb6bedYacdWcsdXc7cWbDda-bmt0Pk-8-slp_zFtpL&aifp=v001 
+    # playpath=mp4:flash/playback/_definst_/kids/astroboy_10_01_22 swfurl=http://www.abc.net.au/iview/images/iview.jpg swfvfy=true
+    return "%s?auth=%s playpath=%s swfurl=%s swfvfy=true" % (auth['rtmp_url'], auth['token'], playpath, config.swf_url)
+
 def play(url):
 
     try:
-        iview_config = comm.get_config()
-        auth = comm.get_auth(iview_config)
-
-        # We don't support Adobe HDS yet, Fallback to RTMP streaming server
-        if auth['rtmp_url'].startswith('http://'):
-            auth['rtmp_url'] = iview_config['rtmp_url'] or config.akamai_fallback_server
-            auth['playpath_prefix'] = config.akamai_playpath_prefix
-            utils.log("Adobe HDS Not Supported, using fallback server %s" % auth['rtmp_url'])
-
         p = classes.Program()
         p.parse_xbmc_url(url)
 
-        # Playpath shoud look like this:
-        #   Akamai: mp4:flash/playback/_definst_/itcrowd_10_03_02
-        playpath = auth['playpath_prefix'] + p.url
-        if playpath.split('.')[-1] == 'mp4':
-            playpath = 'mp4:' + playpath
-    
-        # Strip off the .flv or .mp4
-        playpath = playpath.split('.')[0]
-    
-        # rtmp://cp53909.edgefcs.net/ondemand?auth=daEbjbeaCbGcgb6bedYacdWcsdXc7cWbDda-bmt0Pk-8-slp_zFtpL&aifp=v001 
-        # playpath=mp4:flash/playback/_definst_/kids/astroboy_10_01_22 swfurl=http://www.abc.net.au/iview/images/iview.jpg swfvfy=true
-        rtmp_url = "%s?auth=%s playpath=%s swfurl=%s swfvfy=true" % (auth['rtmp_url'], auth['token'], playpath, config.swf_url)
+        media_url = rtmp_url(p.url)
     
         listitem=xbmcgui.ListItem(label=p.get_list_title(), iconImage=p.thumbnail, thumbnailImage=p.thumbnail)
         listitem.setInfo('video', p.get_xbmc_list_item())
@@ -65,7 +68,7 @@ def play(url):
             listitem.addStreamInfo('audio', p.get_xbmc_audio_stream_info())
             listitem.addStreamInfo('video', p.get_xbmc_video_stream_info())
     
-        xbmc.Player().play(rtmp_url, listitem)
+        xbmc.Player().play(media_url, listitem)
     except:
         # oops print error message
         d = xbmcgui.Dialog()
